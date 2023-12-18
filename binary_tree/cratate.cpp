76fc388b54e15c19afbdbbd80137c8b5c6bc9ba8
1Himanshusharma <himanshusharma272002@gmail.com>
2023-12-18 14:01:46 +0530

    all traversals

--- binary_tree/cratate.cpp
+++ binary_tree/cratate.cpp
@@ -31,7 +31,45 @@ Node *create(){
   return root;
 }
 
+void preorder(Node *root){
+  //base condition
+  if (root==NULL){
+    return;
+  }
+  // follow NLR 
+  cout << root->data << " ";
+  preorder(root->l_child);
+  preorder(root->r_child);
+}
+void postorder(Node *root){
+  //base condition
+  if (root== NULL){
+    return;
+  }
+  // follow LRN
+  postorder(root->l_child);
+  postorder(root->r_child);
+  cout << root->data << " ";
+}
+
+void inorder(Node *root){
+  //base condition
+  if (root == NULL){
+    return;
+  }
+  inorder(root->l_child);
+  cout << root->data << " ";
+  inorder(root->r_child);
+}
+
 int main(){
-  create();
-  
+  Node *root = create();
+  cout << "The preorder traversal: ";
+  preorder(root);
+  cout << endl;
+  cout << "The postorder traversal: ";
+  postorder(root);
+  cout << endl;
+  cout << "The inorder traversal: ";
+  inorder(root);
 }
