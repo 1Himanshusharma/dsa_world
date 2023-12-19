bcd9b81533e75a22fa59181bba4f0b9eddbcbbd3
1Himanshusharma <himanshusharma272002@gmail.com>
2023-12-19 13:35:24 +0530

    find key

--- recursion/find_key.cpp
+++ recursion/find_key.cpp
@@ -11,6 +11,46 @@ using namespace std;
 
 
 bool find(int arr[],int index, int key,int size){
-  //base co
+  //base codition or break conditon 
+  if (index >= size){
+    return false;
+  }
+  // true condition
+  if (arr[index] == key){
+    return true;
+  }
+  bool ans = find(arr,index + 1,key,size);
+  return ans;
+}
 
+//ager mai index ki values ko store karna chata hu
+// store ke liye mujhe vector ya arr chaiye;
+// vector use karenge
+void findindex(int arr[],int index,int size,int key,vector<int>& val){
+  if (index >= size){
+    return;
+  }
+  if (arr[index] == key){
+    val.push_back(index);
+  }
+  findindex(arr,index+1,size,key,val);
+}
+
+int main(){
+  int n;
+  cout << "Plese enter the size of arr: ";
+  cin >> n;
+  int arr[n];
+  for (int i=0;i<n;i++){
+    cin >> arr[i];
+  }
+  int key;
+  cout << "Enter the key value: ";
+  cin >> key;
+  if (find(arr,0,key,n)){
+    cout <<  "true";
+  }
+  else{
+    cout << "false";
+  }
 }
