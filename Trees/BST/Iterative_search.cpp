810a20b064d45d843b2e98116f08367cf3183c06
1Himanshusharma <himanshusharma272002@gmail.com>
2024-01-26 23:02:58 +0530

    bst

---
+++ Trees/BST/Iterative_search.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+Node *insert(Node *p,int key){
+  Node *temp;
+  if (p== NULL){
+    temp = new Node(key);
+    return temp;
+  }
+  if (key < p->data){
+    temp->lchild = 
+  }
+}
