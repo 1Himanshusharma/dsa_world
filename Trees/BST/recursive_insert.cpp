3b7d0694aaf6f487b4e3aba0d3bb251e5a8dcc0c
1Himanshusharma <himanshusharma272002@gmail.com>
2024-01-26 22:51:25 +0530

    bst

--- Trees/BST/recursive_insert.cpp
+++ Trees/BST/recursive_insert.cpp
@@ -1,47 +1,48 @@
 #include<iostream>
 using namespace std;
 
-class Node{
-  public:
+class Node {
+public:
   int data;
   Node *lchild;
   Node *rchild;
-  Node(int key){
+  Node(int key) {
     this->data = key;
     this->lchild = NULL;
     this->rchild = NULL;
   }
 };
 
-Node *insert(Node *p,int key){
-  Node *t;
-  if (p == NULL){
-    t = new Node(key);
-    return t;
+Node* insert(Node* p, int key) {
+  if (p == NULL) {
+    return new Node(key);
   }
-  if (key < p->data){
-    p->lchild = insert(p->lchild,key);
+
+  if (key < p->data) {
+    p->lchild = insert(p->lchild, key);
+  } else if (key > p->data) {
+    p->rchild = insert(p->rchild, key);
   }
-  else if (key > p->data){
-    p->rchild = insert(p->rchild,key);
-  }
-  return t;
+  // If key is equal to p->data, you may choose to handle it accordingly or ignore it.
+
+  return p; // Return the current node
 }
-void preorder(Node *p){
-  if (p){
-    cout << p->data << " ";
+
+void preorder(Node* p) {
+  if (p) {
     preorder(p->lchild);
+    cout << p->data << " ";
     preorder(p->rchild);
   }
 }
-int main(){
-  Node *root = NULL;
-  root = insert(root,50);
-  root = insert(root,30);
-  root = insert(root,20);
-  root = insert(root,40);
-  root = insert(root,10);
-  cout<<"PreOrder Traversal of constructed tree is \n";
+
+int main() {
+  Node* root = NULL;
+  root = insert(root, 50);
+  root = insert(root, 30);
+  root = insert(root, 20);
+  root = insert(root, 40);
+  root = insert(root, 10);
+  cout << "PreOrder Traversal of constructed tree is \n";
   preorder(root);
-  
 }
