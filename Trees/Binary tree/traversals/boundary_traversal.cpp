4a335d871ec431549f4c5a4ebd55a0c12ab071cd
1Himanshusharma <himanshusharma272002@gmail.com>
2024-02-08 16:04:36 +0530

    .

--- Trees/Binary tree/traversals/boundary_traversal.cpp
+++ Trees/Binary tree/traversals/boundary_traversal.cpp
@@ -1,34 +1,127 @@
 #include<iostream>
-using namespace std;
 #include<queue>
-class Node{
-  public:
+using namespace std;
+
+class Node {
+public:
   int data;
   Node *lchild;
-  Noede *rchild;
-  Node(int key){
+  Node *rchild;
+  
+  Node(int key) {
     this->data = key;
     this->lchild = NULL;
     this->rchild = NULL;
   }
 };
-class BT{
-  private:
+
+class BT {
+private:
   Node *root;
-  public:
-  BT(){
+
+  void inorder(Node *p) {
+    if (p) {
+      inorder(p->lchild);
+      cout << p->data << " ";
+      inorder(p->rchild);
+    }
+  }
+  void leftNode(Node *p){
+    if (p== NULL){
+      return;
+    }
+    if (p->lchild == NULL && p->rchild == NULL){
+      return;
+    }
+    cout << p->data << " ";
+    if (p->lchild != NULL){
+      leftNode(p->lchild);
+    }
+    else{
+      leftNode(p->rchild);
+    }
+  }
+  void rightNode(Node *p){
+    if (p== NULL){
+      return;
+    }
+    if (p->lchild != NULL && p->rchild != NULL){
+      return;
+    }
+    cout << p->data << " ";
+    if (p->rchild != NULL){
+      rightNode(p->rchild);
+    }
+    else{
+      rightNode(p->lchild);
+    }
+  }
+  void bottomNode(Node *p){
+    if (p== NULL){
+      return;
+    }
+    if (p->lchild == NULL && p->rchild == NULL){
+      cout << p->data << " ";
+    }
+    bottomNode(p->lchild);
+    bottomNode(p->rchild);
+  }
+
+public:
+  BT() {
     root = NULL;
   }
-  void create(){
+
+  void create() {
     int root_ele;
     cout << "Please Enter the root element: ";
     cin >> root_ele;
-    if (root == NULL){
-      root = new Node(key);
-    }
-    q.push(root);
-    
 
+    if (root == NULL) {
+      root = new Node(root_ele);
+    }
+
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty()) {
+      Node *current_node = q.front();
+      q.pop();
+
+      int key;
+      cout << "Please enter the lchild of " << current_node->data << " : ";
+      cin >> key;
+
+      if (key != -1) {
+        current_node->lchild = new Node(key);
+        q.push(current_node->lchild);
+      }
+
+      cout << "Please enter the rchild of " << current_node->data << " : ";
+      cin >> key;
+
+      if (key != -1) {
+        current_node->rchild = new Node(key);
+        q.push(current_node->rchild);
+      }
+    }
   }
 
+  void inorderTraversal() {
+    inorder(root);
+  }
+  void Boundrytraversal(){
+    leftNode(root);
+    bottomNode(root);
+    rightNode(root->rchild);
+
+  }
+};
+
+int main() {
+  BT t;
+  t.create();
+  cout << "The boundry traversal: ";
+  t.Boundrytraversal();
+  return 0;
 }
